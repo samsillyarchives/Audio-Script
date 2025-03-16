694fe26fe9c3ba5fb04654a806d31e5ed87a89ef
samsillyarchives <sml9740@nyu.edu>
2025-03-16 18:06:36 -0400

    javascript and sound files

--- script.js
+++ script.js
@@ -1,5 +1,49 @@
 
 const Crash = function() {
-    var crashaudio = new Audio('crashsound.wav')
+    var crashaudio = new Audio('crashsound.wav');
     crashaudio.play();
+}
+
+const Tom1 = function() {
+    var tom1sound = new Audio('tom1sound.wav');
+    tom1sound.play();
+}
+
+const HiHat = function() {
+    var hihatsound = new Audio('hihatsound.wav');
+    hihatsound.play();
+}
+
+const Snare = function(){
+    var snaresound = new Audio('snaresound.wav');
+    snaresound.play()
+}
+
+const Tom2 = function(){
+    var tom2sound = new Audio('tom2sound.wav');
+    tom2sound.play();
+}
+
+const Tom3 = function(){
+    var tom3sound =  new Audio('tom3sound.wav');
+    tom3sound.play();
+}
+
+const Ride = function(){
+    var ridesound = new Audio ('ridesound.wav');
+    ridesound.play();
+}
+const Tom4 = function(){
+    var tom4sound = new Audio('tom4sound.wav');
+    tom4sound.play();
+}
+
+const Crash2 = function() {
+    var crashaudio = new Audio('crashsound.wav');
+    crashaudio.play();
+}
+
+const Kick = function(){
+    var kickdrumsound = new Audio('kickdrumsound.wav');
+    kickdrumsound.play();
 }
